8f503298e42a9b2822d507bd8c2cd689a0462f4d
Natasha Stepanova <aleksashkina@yahoo.com>
2025-05-15 17:47:01 +0300

    Update and rename sql_2 to sql_2.sql

---
+++ sql_2.sql
@@ -0,0 +1,10 @@
+--Задача 2:
+--Вывести список контактов за сегодня. Указать тип контакта, код оператора, ФИО должника, остаток по долгу и проценты. Отсортировать результат по указанным полям. Порядок сортировки совпадает с порядком полей.
+
+--Решение:
+SELECT c.Typ, c.Operator, p.LASTNAME, p.FIRSTNAME, p.MIDNAME, d.Ostat, d.Debt_sym
+FROM contact_log c
+JOIN debt d ON c.Parent_id = d.Parent_id
+JOIN person p ON d.Parent_id = p.ID
+WHERE c.Date = CURRENT_DATE
+ORDER BY c.Typ, c.Operator, p.LASTNAME, p.FIRSTNAME, p.MIDNAME, d.Ostat, d.Debt_sym;
